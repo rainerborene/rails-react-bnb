abd0e1b79db64aa955c38352c1e3fc167d3b099a
Rainer Borene <rainerborene@gmail.com>
2016-10-10 10:45:19 -0300

    Setup Twitter Bootstrap.

--- app/assets/javascripts/components/Main.js
+++ app/assets/javascripts/components/Main.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
 
 class Main extends React.Component {
 
   render() {
     return (
-      <h1>App</h1>
+      <div className="container">
+        <h1>House</h1>
+
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Address</th>
+              <th>Zip</th>
+              <th>City</th>
+              <th>State</th>
+              <th>Number Of Bedrooms</th>
+              <th></th>
+            </tr>
+          </thead>
+        </Table>
+      </div>
     );
   }
 
