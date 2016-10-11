c55d876745ae9b68be087c9c4bb5e3f26d8e4710
Rainer Borene <rainerborene@gmail.com>
2016-10-11 11:27:19 -0300

    Bundle libraries in another file.

---
+++ app/assets/javascripts/libs.js
@@ -0,0 +1,10 @@
+//= require_self
+
+require('react');
+require('react-bootstrap');
+require('react-dom');
+require('react-redux');
+require('react-router');
+require('react-router-redux');
+require('redux');
+require('redux-thunk');
